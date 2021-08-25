36fc99d22a380aeb69f9f4be226e0fa4831bb08b
Vitorio Venturi <v.venturi@yahoo.com.br>
2021-08-25 19:45:34 -0300

    Commit Inicial

---
+++ src/app/filmes/cadastro-filmes/cadastro-filmes.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'dio-cadastro-filmes',
+  templateUrl: './cadastro-filmes.component.html',
+  styleUrls: ['./cadastro-filmes.component.scss']
+})
+export class CadastroFilmesComponent implements OnInit {
+
+  cadastro: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  get f() {
+    return this.cadastro.controls;
+  }
+
+  ngOnInit() {
+
+    this.cadastro = this.fb.group({
+      titulo: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(256)]],
+      urlFoto: ['', [Validators.minLength(10)]],
+      dtLancamento: ['', [Validators.required]],
+      descricao: [''],
+      nota: [0, [Validators.required, Validators.min(0), Validators.max(10)]],
+      urlIMDB: ['' ,[Validators.minLength(10)]],
+      genero: ['', [Validators.required]],
+
+    });
+
+  }
+
+  salvar(): void {
+    this.cadastro.markAllAsTouched();
+    if (this.cadastro.invalid){
+      return;
+    }
+    alert('Cadastrado com sucesso!!\n\n' + JSON.stringify(this.cadastro.value, null, 4));
+  }
+
+  reiniciarForm(): void{
+    this.cadastro.reset();
+  }
+}
